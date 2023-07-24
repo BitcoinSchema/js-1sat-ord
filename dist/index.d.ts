9b190347d57374a937186588680e2b3a66dc50b3
Luke <rohenaz@gmail.com>
2023-07-23 21:10:42 -0400

    move dotenv to dev dependency

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -24,7 +24,7 @@ export type MAP = {
     [prop: string]: string | string[];
 };
 declare const buildInscription: (destinationAddress: P2PKHAddress, b64File?: string | undefined, mediaType?: string | undefined, metaData?: MAP | undefined) => Script;
-export declare const reinscribeOrdinalTemplate: (ordinal: Utxo, destinationAddress: string, reinscription?: Inscription, metaData?: MAP) => Promise<Transaction>;
+export declare const buildReinscriptionTemplate: (ordinal: Utxo, destinationAddress: string, reinscription?: Inscription, metaData?: MAP) => Promise<Transaction>;
 declare const createOrdinal: (utxo: Utxo, destinationAddress: string, paymentPk: PrivateKey, changeAddress: string, satPerByteFee: number, inscription: Inscription, metaData?: MAP, signer?: LocalSigner | RemoteSigner) => Promise<Transaction>;
 declare const sendOrdinal: (paymentUtxo: Utxo, ordinal: Utxo, paymentPk: PrivateKey, changeAddress: string, satPerByteFee: number, ordPk: PrivateKey, ordDestinationAddress: string, reinscription?: Inscription, metaData?: MAP) => Promise<Transaction>;
 declare const sendUtxos: (utxos: Utxo[], paymentPk: PrivateKey, address: P2PKHAddress, feeSats: number) => Promise<Transaction>;
