46bcddaef4b2f98dc303c72a8726b3b25b4842d5
Satchmo <rohenaz@gmail.com>
2024-07-03 08:56:59 -0400

    fix toHex utility function to use buffer

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -1,23 +1,5 @@
 const toHex = (asciiStr: string) => {
-  const arr1: string[] = [];
-  for (let n = 0, l = asciiStr.length; n < l; n++) {
-    const hex = Number(asciiStr.charCodeAt(n)).toString(16);
-    arr1.push(hex);
-  }
-  return arr1.join("");
+  return Buffer.from(asciiStr).toString("hex");
 };
 
-const toAscii = (hexStr: string) => {
-  let hex: string;
-  let i: number;
-
-  let result = "";
-  for (i = 0; i < hexStr.length; i++) {
-    hex = hexStr.charCodeAt(i).toString(16);
-    result += (`000${hex}`).slice(-4);
-  }
-
-  return result;
-};
-
-export { toHex, toAscii };
+export { toHex };
