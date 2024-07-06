842161ac6a0b466bf1a42e1612856fe8c3b9839e
Satchmo <rohenaz@gmail.com>
2024-07-06 01:52:04 -0400

    update types export

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,5 +3,4 @@ import { sendOrdinals } from "./sendOrdinals";
 import { sendUtxos } from "./sendUtxos";
 import { transferOrdTokens } from "./transferOrdinals";
 import { fetchPayUtxos } from "./utils/utxo";
-import "./types";
 export { createOrdinals, sendOrdinals, sendUtxos, transferOrdTokens, fetchPayUtxos };
