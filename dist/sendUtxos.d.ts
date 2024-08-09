64a94406151b62b90401321e9380f4b02f5b056d
Satchmo <rohenaz@gmail.com>
2024-08-09 11:18:41 -0400

    build

--- dist/sendUtxos.d.ts
+++ dist/sendUtxos.d.ts
@@ -5,8 +5,8 @@ import type { ChangeResult, SendUtxosConfig } from "./types";
  * @param {Utxo[]} config.utxos - Utxos to spend (with base64 encoded scripts)
  * @param {PrivateKey} config.paymentPk - Private key to sign utxos
  * @param {Payment[]} config.payments - Array of payments with addresses and amounts
- * @param {number} [config.satsPerKb] - Satoshis per kilobyte for fee calculation. Default is DEFAULT_SAT_PER_KB
- * @param {string} [config.changeAddress] - Address to send change to. If not provided, defaults to paymentPk address
+ * @param {number} [config.satsPerKb] - (Optional) Satoshis per kilobyte for fee calculation. Default is DEFAULT_SAT_PER_KB
+ * @param {string} [config.changeAddress] - (Optional) Address to send change to. If not provided, defaults to paymentPk address
  * @param {string} [config.metaData] - (Optional) Metadata to include in OP_RETURN of the payment output
  * @returns {Promise<ChangeResult>} Transaction with utxo outputs
  */
