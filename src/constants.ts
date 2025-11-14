175c13020b099f7ee23331a4f5a70c9a90d6d819
Luke Rohenaz <rohenaz@gmail.com>
2025-11-14 10:54:15 -0500

    Increase default fee rate from 10 to 100 sat/kb

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const MAP_PREFIX = "1PuQa7K62MiKCtssSLKy1kh56WWU7MtUR5";
-export const DEFAULT_SAT_PER_KB = 10;
+export const DEFAULT_SAT_PER_KB = 100;
 export const API_HOST = "https://ordinals.gorillapool.io/api";
